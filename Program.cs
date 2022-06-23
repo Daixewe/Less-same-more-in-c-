ca75e7f4c00fa1ee106d7af5e36d46cd8cad69e9
Daixewe <fuentesvd56@gmail.com>
2022-06-22 22:55:50 -0600

    Less,same or more than 5

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("less,same or more");
+
+Console.WriteLine("Choose a number");
+int numeroUno = int.Parse(Console.ReadLine());
+
+if (numeroUno > 5)
+{
+    Console.WriteLine("More than 5");
+} else if (numeroUno == 5)
+{
+    Console.WriteLine("Is the same number");
+} else
+{
+    Console.WriteLine("Less than 5");
+}
+
+
+
+
+
+
